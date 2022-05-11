5900369079df20a9fd2a047fadf308fc387f50d1
mightyRam <khovachina@gmail.com>
2022-05-11 12:55:04 +0200

    feat: added toastify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
   const [toggle, setToggle] = useState(true);
+  useEffect(() => {
+    if (toggle === true) {
+      toast.success("ToggleOn");
+    } else {
+      toast.warn("ToggleOff");
+    }
+  });
 
   return (
     <div className="App">
@@ -24,6 +33,7 @@ function App() {
           <Button color="danger">Danger!</Button>
         </div>
       </header>
+      <ToastContainer autoClose={3000} hideProgressBar />
     </div>
   );
 }
