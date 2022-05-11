362e6628f8a549ca2662373d6ea1abba9b6929c9
mightyRam <khovachina@gmail.com>
2022-05-11 12:14:50 +0200

    feat: added react strap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [toggle, setToggle] = useState(true);
@@ -18,6 +20,9 @@ function App() {
         >
           Button
         </button>
+        <div>
+          <Button color="danger">Danger!</Button>
+        </div>
       </header>
     </div>
   );
