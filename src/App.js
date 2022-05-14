c102e0a68a5c0068f4a73c4602edfc27a609b32e
mightyRam <khovachina@gmail.com>
2022-05-14 13:23:46 +0200

    feat: router fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
@@ -11,18 +11,20 @@ import { AboutPage } from "./components/AboutPage";
 
 function App() {
   return (
-    <div className="App">
-      <AppBar />
-      <div className="App-header">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="Contact" element={<ContactPage />} />
-          <Route path="About" element={<AboutPage />} />
-        </Routes>
-      </div>
+    <Router>
+      <div className="App">
+        <AppBar />
+        <div className="App-header">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/Contact" element={<ContactPage />} />
+            <Route path="/About" element={<AboutPage />} />
+          </Routes>
+        </div>
 
-      <ToastContainer autoClose={3000} hideProgressBar />
-    </div>
+        <ToastContainer autoClose={3000} hideProgressBar />
+      </div>
+    </Router>
   );
 }
 
